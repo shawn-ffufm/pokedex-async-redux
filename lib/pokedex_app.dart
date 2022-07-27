b3ce0167f083af52ca5221272d6ddeabe9df7954
Yohan Cai <yohan.cai@ffuf.de>
2022-07-27 14:44:10 +0800

    [PKM-9] feat: Async Redux setup
    - Updated pokemon list directory
    - Updated naming conventions
    - Transfered Storeprovider on main
    - Created a string.dart file

--- lib/pokedex_app.dart
+++ lib/pokedex_app.dart
@@ -1,7 +1,5 @@
-import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/features/pokemon_list/pokemon_list_connector.dart';
-import 'package:pokedex/state/app_state.dart';
 
 class PokedexApp extends StatelessWidget {
   const PokedexApp({Key? key}) : super(key: key);
