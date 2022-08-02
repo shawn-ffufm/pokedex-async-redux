1848c43fd4cf8496782bcfdb646148516b3cd8b8
Yohan Cai <yohan.cai@ffuf.de>
2022-08-02 14:58:32 +0800

    [PKM-29] feat: Api Client & service setup
    - revised api url

--- lib/api/pokedex_api_spec/handler/pokemon_handler.dart
+++ lib/api/pokedex_api_spec/handler/pokemon_handler.dart
@@ -1,19 +1,18 @@
 import 'package:pokedex/api/pokedex_api_spec/handler/api_client.dart';
 import 'package:pokedex/api/pokedex_api_spec/model/pokemon_model.dart';
-import 'package:pokedex/utils/constant.dart' as k;
 
 class PokemonApi {
   final ApiClient apiClient;
 
   PokemonApi(this.apiClient);
 
-  static const int limitAmount = 20;
+  static const int _limitAmount = 20;
 
   Future<PokemonModel> getPokemonList() async {
     final queryParams = <String, dynamic>{};
 
     final baseUri = Uri.parse(apiClient.baseUrl);
-    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}${k.apiLimit}$limitAmount');
+    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}/?limit=$_limitAmount');
 
     return apiClient.dio.getUri(uri).then((response) {
       return response.data.map((dynamic e) => PokemonModel(name: e['name']));
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,4 +3,3 @@ const String imageURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/maste
 const int homeTabIndex = 0;
 const int favoriteTabIndex = 1;
 const String baseApi = 'https://pokeapi.co/api/v2/pokemon/';
-const String apiLimit = '/?limit=';
