5dd349d08e82441ee5658b3dbba778600339f1e6
Yohan Cai <yohan.cai@ffuf.de>
2022-08-03 15:25:52 +0800

    [PKM-10] Presentation Layer Setup
    - initial view of Pokemon List

--- lib/features/pokemon_overview/pokemon_overview_page.dart
+++ lib/features/pokemon_overview/pokemon_overview_page.dart
@@ -7,5 +7,72 @@ class PokemonOverviewPage extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => const Center(child: Text(str.pokedexTitle));
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(str.pokedexTitle),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(20, (index) {
+            return GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child: Stack(
+                  children: const [
+                    Positioned(
+                      left: 5,
+                      top: 5,
+                      child: Text(
+                        str.nameFiller,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Icon(
+                        Icons.catching_pokemon,
+                        size: 120,
+                      ),
+                    ),
+                    Positioned(
+                      left: 5,
+                      bottom: 100,
+                      child: Text(
+                        str.typeFiller,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 5,
+                      bottom: 70,
+                      child: Text(
+                        str.typeFiller,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
 }
--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,3 +1,7 @@
 const String homeTitle = 'Home';
 const String pokedexTitle = 'Pokedex';
 const String favoriteTitle = 'Favorites';
+
+/// TODO: This string will be deleted once pokemon name is available
+const String nameFiller = 'Lorem name';
+const String typeFiller = 'Lorem type';
