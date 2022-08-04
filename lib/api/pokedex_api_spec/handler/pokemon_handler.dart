02823e99fd03e30c04c3fcd127551ecc50f00cf9
Yohan Cai <yohan.cai@ffuf.de>
2022-08-04 16:57:38 +0800

    [PKM-11] feat: Data layer setup
    - string constant

--- lib/api/pokedex_api_spec/handler/pokemon_handler.dart
+++ lib/api/pokedex_api_spec/handler/pokemon_handler.dart
@@ -1,5 +1,6 @@
 import 'package:pokedex/api/api_client.dart';
 import 'package:pokedex/api/pokedex_api_spec/model/pokemon_model.dart';
+import 'package:pokedex/utils/strings.dart' as str;
 
 class PokemonApi {
   final ApiClient apiClient;
@@ -10,9 +11,9 @@ class PokemonApi {
     final queryParams = <String, dynamic>{};
 
     final baseUri = Uri.parse(apiClient.dio.options.baseUrl);
-    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}/pokemon/');
+    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}/${str.pokemonRoute}/');
     return await apiClient.dio
         .getUri(uri)
-        .then((response) => response.data['results'].map<Pokemon>((pokemon) => Pokemon.fromJson(pokemon)).toList());
+        .then((response) => response.data[str.results].map<Pokemon>((pokemon) => Pokemon.fromJson(pokemon)).toList());
   }
 }
--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,6 +1,8 @@
 const String homeTitle = 'Home';
 const String pokedexTitle = 'Pokedex';
 const String favoriteTitle = 'Favorites';
+const String results = 'results';
+const String pokemonRoute = 'pokemon';
 
 /// TODO: This string will be deleted once pokemon name is available
 const String nameFiller = 'Lorem name';
