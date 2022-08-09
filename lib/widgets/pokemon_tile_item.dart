a6cd8aedcfc54e2796cea9602f835a7d372ae9cb
Yohan Cai <yohan.cai@ffuf.de>
2022-08-09 13:00:27 +0800

    [PKM-12] feat: Integration of Presentation & Data Layers

--- lib/widgets/pokemon_tile_item.dart
+++ lib/widgets/pokemon_tile_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:pokedex/api/pokedex_api_spec/model/pokemon.dart';
 import 'package:pokedex/utils/constant.dart' as k;
 import 'package:dartx/dartx.dart';
